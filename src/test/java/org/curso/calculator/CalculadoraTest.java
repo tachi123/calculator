768dca903b629fd98049329a3fb5e3c93c22dfb2
Jaime <jnramirezlorca@gmail.com>
2023-10-10 16:49:23 -0300

    se suben los casos de prueba faltante

--- src/test/java/org/curso/calculator/CalculadoraTest.java
+++ src/test/java/org/curso/calculator/CalculadoraTest.java
@@ -1,10 +1,12 @@
 package org.curso.calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import org.junit.Assert;
 
 public class CalculadoraTest {
 
@@ -44,13 +46,36 @@ public class CalculadoraTest {
 	}
 	
 	//2. El resultado de sumar 150 y 180, dividido por 3, da 110
-	
-	
+	@Test
+	public void resultadoDeLaSuma_DivididoTres()
+	{
+		unNumero = 150;
+		otroNumero = 180;
+		resultadoEsperado = Calculadora.dividir(Calculadora.sumar(unNumero,otroNumero),3);
+		assertEquals(110, resultadoEsperado);
+	}
+		
 	//3. El resultado de restar 90 y 50, multiplicado por 15, no da 605.
-	
-	
+	@Test
+	public void laMultiplicacionDeLaResta()
+	{
+		unNumero = 90;
+		otroNumero = 50;
+		resultadoEsperado = Calculadora.multiplicar(Calculadora.restar(unNumero,otroNumero),15);
+		//Ponemos .0 para que java infiera que el tipo de dato es double
+		assertNotEquals(resultadoEsperado,605);
+	}
+		
 	//4. El resultado de sumar 70 y 40, multiplicado por 25, no da 2700.
-
+	@Test
+	public void laSumaYMultiplicacion()
+	{
+		unNumero = 70;
+		otroNumero = 40;
+		resultadoEsperado = Calculadora.multiplicar(Calculadora.sumar(unNumero,otroNumero),25);
+		//Ponemos .0 para que java infiera que el tipo de dato es double
+		assertNotEquals(2700.0,resultadoEsperado);
+	}
 	
 	
 }
